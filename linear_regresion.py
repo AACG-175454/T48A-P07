9435333829383c86088c4854a06eabc24c1551b0
Jesús Alberto Revilla Silva <jesus_revilla@yahoo.com>
2025-09-26 18:35:02 -0600

    Update linear_regresion.py

--- linear_regresion.py
+++ linear_regresion.py
@@ -1,1 +1,9 @@
+import unittest
+import numpy as np
+import pandas as pd
+from linear_regresion import linear_regresion
 
+class TestPandasExercises(unittest.TestCase):
+    def test_linear_regresion(unittest.TestCase):
+        mse, r2 = linear_regresion() 
+        assert np.isclose(mse, 4198.86,  1) and np.isclose(r2, 0.23, 0.2)
